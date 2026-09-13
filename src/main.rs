6a01256dea09391fbfc842f032b37a540929b2fb
Yahddyyp <Yahddyyp@gmail.com>
2026-09-13 17:45:05 +0530

    Made get_user_info return UserProfile

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
 use std::env;
 
+use octocrab::models::UserProfile;
+
 #[allow(dead_code)]
 #[derive(Debug)]
 struct UserInfo {
@@ -14,7 +16,7 @@ struct UserInfo {
     id: i64,
 }
 
-async fn get_user_info(args: Vec<String>) -> Result<(), Box<dyn std::error::Error>> {
+async fn get_user_info(args: Vec<String>) -> Result<UserProfile, Box<dyn std::error::Error>> {
     if args.len() >= 3 {
         eprintln!("Cannot take more than two arguments");
         std::process::exit(1);
@@ -25,33 +27,33 @@ async fn get_user_info(args: Vec<String>) -> Result<(), Box<dyn std::error::Erro
     let octocrab = octocrab::Octocrab::builder().build()?;
 
     match octocrab.users(username).profile().await {
-        Ok(user) => {
-            let user_info = UserInfo {
-                name: user.login,
-                followers: user.followers.try_into()?,
-                created_at: user.created_at.format("%d-%m-%Y at %I:%M %p").to_string(),
-                public_repos: user.public_repos,
-                twitter_user: user.twitter_username.unwrap_or_else(|| "none".to_string()),
-                company: user.company.unwrap_or_else(|| "Unemployed".to_string()),
-                location: user.location.unwrap_or("No where".to_string()),
-                id: user.id.0.try_into()?,
-                avater_url: user.avatar_url.to_string(),
-            };
-            println!("{user_info:#?}");
-        }
+        Ok(user) => Ok(user),
         Err(e) => {
             eprintln!("Error: user '{}' not found ({})", username, e);
             std::process::exit(1);
         }
     }
-    Ok(())
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
 
-    get_user_info(args).await?;
+    let user = get_user_info(args).await?;
+
+    let user_info = UserInfo {
+        name: user.login,
+        followers: user.followers.try_into()?,
+        created_at: user.created_at.format("%d-%m-%Y at %I:%M %p").to_string(),
+        public_repos: user.public_repos,
+        twitter_user: user.twitter_username.unwrap_or_else(|| "none".to_string()),
+        company: user.company.unwrap_or_else(|| "Unemployed".to_string()),
+        location: user.location.unwrap_or("No where".to_string()),
+        id: user.id.0.try_into()?,
+        avater_url: user.avatar_url.to_string(),
+    };
+
+    println!("{user_info:#?}");
 
     Ok(())
 }
