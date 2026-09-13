367f03a3ee58012bc502f42f70dab34394681350
Yahddyyp <Yahddyyp@gmail.com>
2026-09-13 18:02:13 +0530

    Handled no wifi

--- src/main.rs
+++ src/main.rs
@@ -28,8 +28,21 @@ async fn get_user_info(args: Vec<String>) -> Result<UserProfile, Box<dyn std::er
 
     match octocrab.users(username).profile().await {
         Ok(user) => Ok(user),
+
+        Err(octocrab::Error::Service { source, .. }) if source.to_string().contains("Connect") => {
+            eprintln!("No wifi");
+            std::process::exit(1);
+        }
+
+        Err(octocrab::Error::GitHub { source, .. })
+            if source.status_code == reqwest::StatusCode::NOT_FOUND =>
+        {
+            eprintln!("user '{}' not found.", username);
+            std::process::exit(1);
+        }
+
         Err(e) => {
-            eprintln!("Error: user '{}' not found ({})", username, e);
+            eprintln!("Unexpected error: {}", e);
             std::process::exit(1);
         }
     }
