f06969ceec42a9822f6beb8ec274d5117fcce652
Yahddyyp <Yahddyyp@gmail.com>
2026-09-13 19:08:40 +0530

    feat: Calculate sum of stars

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 
-use octocrab::models::UserProfile;
+use anyhow::Result;
+use octocrab::{Octocrab, models::UserProfile};
 
 #[allow(dead_code)]
 #[derive(Debug)]
@@ -14,26 +15,57 @@ struct UserInfo {
     company: String,
     location: String,
     id: i64,
+    bio: String,
+    blog: String,
+    total_stars: u32,
 }
 
-async fn get_user_info(args: Vec<String>) -> Result<UserProfile, Box<dyn std::error::Error>> {
+/// Sum up all the stars of the public repos
+async fn get_total_stars(octocrab: &octocrab::Octocrab, username: &str) -> octocrab::Result<u32> {
+    let mut current_page = octocrab
+        .users(username)
+        .repos()
+        .per_page(100)
+        .send()
+        .await?;
+
+    let mut all_repos = current_page.take_items();
+
+    while let Ok(Some(mut next_page)) = octocrab.get_page(&current_page.next).await {
+        all_repos.extend(next_page.take_items());
+        current_page = next_page;
+    }
+
+    let total_stars: u32 = all_repos
+        .iter()
+        .map(|r| r.stargazers_count.unwrap_or(0))
+        .sum();
+
+    Ok(total_stars)
+}
+
+/// Build the client instance and get the info
+async fn get_user_info(
+    args: &Vec<String>,
+    username: &str,
+    octocrab: &Octocrab,
+) -> Result<UserProfile, Box<dyn std::error::Error>> {
     if args.len() >= 3 {
         eprintln!("Cannot take more than two arguments");
         std::process::exit(1);
     }
 
-    let username = args.get(1).ok_or("no username provided")?;
-
-    let octocrab = octocrab::Octocrab::builder().build()?;
-
     match octocrab.users(username).profile().await {
+        // If user actually exists
         Ok(user) => Ok(user),
 
+        // No wifi
         Err(octocrab::Error::Service { source, .. }) if source.to_string().contains("Connect") => {
             eprintln!("No wifi");
             std::process::exit(1);
         }
 
+        // User was not found
         Err(octocrab::Error::GitHub { source, .. })
             if source.status_code == reqwest::StatusCode::NOT_FOUND =>
         {
@@ -41,6 +73,7 @@ async fn get_user_info(args: Vec<String>) -> Result<UserProfile, Box<dyn std::er
             std::process::exit(1);
         }
 
+        // IDK what happened
         Err(e) => {
             eprintln!("Unexpected error: {}", e);
             std::process::exit(1);
@@ -51,19 +84,33 @@ async fn get_user_info(args: Vec<String>) -> Result<UserProfile, Box<dyn std::er
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args: Vec<String> = env::args().collect();
+    let username = args.get(1).ok_or("no username provided")?;
 
-    let user = get_user_info(args).await?;
+    // Build the client instance
+    let octocrab = octocrab::Octocrab::builder().build()?;
+
+    let user = get_user_info(&args, &username, &octocrab).await?;
+
+    let total_stars_var = get_total_stars(&octocrab, &username).await?;
 
     let user_info = UserInfo {
         name: user.login,
         followers: user.followers.try_into()?,
+        total_stars: total_stars_var,
         created_at: user.created_at.format("%d-%m-%Y at %I:%M %p").to_string(),
         public_repos: user.public_repos,
-        twitter_user: user.twitter_username.unwrap_or_else(|| "none".to_string()),
+        blog: user.blog.unwrap_or_else(|| "NOT_FOUND".to_string()),
+        twitter_user: user
+            .twitter_username
+            .unwrap_or_else(|| "NOT_FOUND".to_string()),
         company: user.company.unwrap_or_else(|| "Unemployed".to_string()),
         location: user.location.unwrap_or("No where".to_string()),
         id: user.id.0.try_into()?,
         avater_url: user.avatar_url.to_string(),
+        bio: user
+            .bio
+            .map(|b| b.split_whitespace().collect::<Vec<_>>().join(" "))
+            .unwrap_or_else(|| "NOT_FOUND".to_string()),
     };
 
     println!("{user_info:#?}");
